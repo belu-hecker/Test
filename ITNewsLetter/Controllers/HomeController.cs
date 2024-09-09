e837cdfe08f52cd0d85f454c39b25b844a9cee79
GAURAV VYAS <20353976@LTDIC.com>
2024-09-09 14:22:13 +0530

    first commit

---
+++ ITNewsLetter/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ITNewsLetter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ITNewsLetter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.vbMenuActive = "1";
+            return View();
+        }
+        public IActionResult FeedBack()
+        {
+            ViewBag.vbMenuActive = "2";
+            return View();
+        }
+        public IActionResult SepAddition()
+        {
+            ViewBag.vbMenuActive = "1";
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ITNewsLetter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITNewsLetter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
